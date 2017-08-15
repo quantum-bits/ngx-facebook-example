8c4052b7fb3df7a9a2678fb508f4be03fe440b0b
Ken Kiers <knkiers@taylor.edu>
2017-08-15 15:16:08 -0400

    add new route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {RouterModule, Routes} from "@angular/router";
+
 import { AppComponent } from './app.component';
 
 import { FacebookModule } from 'ngx-facebook';
+import { NewRouteComponent } from './new-route/new-route.component';
+
+
+// WORKING HERE: add a new route....
+
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewRouteComponent
   ],
   imports: [
     BrowserModule,
