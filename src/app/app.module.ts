c5540cde072de93b8d7b86796ffc803b8fddf8eb
Ken Kiers <knkiers@taylor.edu>
2017-08-15 15:26:29 -0400

    add path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 
 import { FacebookModule } from 'ngx-facebook';
 
+const routes: Routes = [
+  path: 'new-route', component: EditReadingResourcesComponent
+  {path: '', redirectTo: '/end-user/readings/today', pathMatch: 'full'},
+  { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' }
+];
 
 @NgModule({
   declarations: [
